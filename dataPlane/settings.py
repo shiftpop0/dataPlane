b3061fd1212a24ab7b44610c8e3de34f70c1a506
Xinze Wu <79691045+shiftpop0@users.noreply.github.com>
2024-11-19 15:56:36 +0800

    Update settings.py
    Time_zone

--- dataPlane/settings.py
+++ dataPlane/settings.py
@@ -123,11 +123,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "zh-hans"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/ShangHai"
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
